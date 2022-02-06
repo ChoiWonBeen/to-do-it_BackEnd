1e8b61e7cff9d90085cdacccc46d3609b57fabd6
최 원빈 <zad1264@naver.com>
2022-02-06 12:57:08 +0000

    add libraries

---
+++ db/index.js
@@ -0,0 +1,12 @@
+const mysql = require("mysql");
+const host = "localhost";
+require('dotenv').config()
+
+module.exports = {
+  connection: mysql.createConnection({
+    host: host,
+    user: "root",
+    password: process.env.DB_PASSWORD,
+    database: "chat",
+  }),
+};
