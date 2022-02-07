202d3ee98d35e75ba6991a3ced78347096d75109
최 원빈 <zad1264@naver.com>
2022-02-07 07:52:26 +0000

    solve cors issue

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const mysql = require("mysql");
 const db = require("./db/index.js");
+const cors = require("cors")
 
 const app = express();
-
 app.set('port', 3000);
 
-app.get("/users", (req, res) => {
+var corsOptions = {
+  origin: '*',
+  optionsSuccessStatus: 200
+}
+
+app.get("/users", cors(corsOptions), (req, res) => {
     db.connection.query( `SELECT * FROM users`, (err, results)=>{
         if(err)
           console.log(err);
